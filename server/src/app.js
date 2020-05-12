0a5599e1448e07b95c182cbf863d07b8674be913
Mari Kay <marikayhannon@gmail.com>
2020-05-12 17:18:30 -0400

    setting up the registration page

--- server/src/app.js
+++ server/src/app.js
@@ -4,3 +4,13 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/register', (_req,res) => {
+res.send({
+    message: 'Hi ${_req.body.email} your user was registered'
+})
+})
+app.listen(process.env.PORT || 8081 )
